326d0e04cd389efa8c43b6f1403a3f2d733a121f
pratik2405 <shinde7388pratik@gmail.com>
2024-11-14 10:49:45 +0530

    Add project files.

---
+++ GenericClassMethod/GenericMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassMethod
+{
+    public class GenericMethod
+    {
+
+        public void Swipe<T>(T a , T b)
+        {
+           Console.WriteLine($"a={a} b={b}");
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"a={a}  b={b}");
+        }
+    }
+}
---
+++ GenericClassMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<int> gc=new GenericClass<int>();
+            gc.Add(1);
+            Console.WriteLine(gc.Get());
+
+            GenericClass<string>gc2=new GenericClass<string>();
+            gc2.Add("Pratik");
+            Console.WriteLine(gc2.Get());
+
+            GenericClass<Product>gc3= new GenericClass<Product>();
+            gc3.Add(new Product(1, "milk"));
+            Console.WriteLine(gc3.Get());
+
+            Console.WriteLine("--------------------------------------");
+
+            GenericMethod gm=new GenericMethod();
+            gm.Swipe(5, 10);
+            Console.WriteLine();
+        }
+    }
+}
